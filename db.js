3cea36e255a61e79acbc7ade1d2333c0c784ad2e
Saad <saadmasood96@hotmail.com>
2020-06-07 14:29:42 -0400

    update README, db.js formatting

--- db.js
+++ db.js
@@ -1,41 +1,34 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
-const dotenv = require('dotenv');
-dotenv.config();
-
-
-// Schemas //
-
 const TrackSchema = new mongoose.Schema({
-    title: {type: String, required: true}, 
-    spotifyID: {type: String, required: true},
-    artists: {type: [String], required: true},
+    title: { type: String, required: true }, 
+    spotifyID: { type: String, required: true },
+    artists: { type: [String], required: true },
 });
 mongoose.model('Track', TrackSchema);
 
 const ArtistSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    spotifyID: {type: String, required: true}
+    name: { type: String, required: true },
+    spotifyID: { type: String, required: true }
 });
 mongoose.model('Artist', ArtistSchema);
 
 const UserSchema = new mongoose.Schema({
-    spotifyID: {type: String, required: true},
+    spotifyID: { type: String, required: true },
     name: String,
-    token: {type: String, required: true},
-    
+    token: { type: String, required: true },
     topTracks_long: [TrackSchema],
     topTracks_med: [TrackSchema],
     topTracks_short: [TrackSchema],
-
     topArtists_long: [ArtistSchema],
     topArtists_med: [ArtistSchema],
     topArtists_short: [ArtistSchema],
 });
 mongoose.model('User', UserSchema);
-
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/metrify', { useNewUrlParser: true });
