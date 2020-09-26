f67eb99b60d52092a754b3314d6f5e8e014e0ec2
Saad <saadmasood96@hotmail.com>
2020-09-26 16:10:38 -0400

    fetch audio features

--- public/scripts/track-features.js
+++ public/scripts/track-features.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", main);
 
 function main() {
-  console.log('fiasnfias')
-  // getTrackFeatures();
+  getTrackFeatures();
 }
 
 async function getTrackFeatures() {
   const trackId = document.location.pathname.split('/track/')[1];
   const rawRes = await fetch(`http://localhost:3000/track-features/${trackId}`);
   const audioFeatures = await rawRes.json();
-  console.log(audioFeatures);
 }
